e46347447a26a312a5fe1ecaf6ccfacea96246df
Harold Rodriguez <haroldkt@hotmail.com>
2019-09-19 22:53:04 -0400

    added babel polyfills and first function component with react hooks

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './lib/components/index.js',
+  entry: ['@babel/polyfill', './lib/components/index.js'],
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
