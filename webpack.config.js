b754008b2a198371e056cbc01fb21d0e693b80e5
Harold Rodriguez <haroldkt@hotmail.com>
2019-09-19 22:27:14 -0400

    added react, updated babel to v7

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './lib/components/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+};
