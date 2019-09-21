2d9a4792e3c3442cac202b0e280dc285521b7170
Harold Rodriguez <haroldkt@hotmail.com>
2019-09-21 13:57:38 -0400

    added articles and article-list components, retrieving data from api

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: ['@babel/polyfill', './lib/components/index.js'],
+  entry: ['@babel/polyfill', './lib/renderers/dom.js'],
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
